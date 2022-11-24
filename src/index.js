e7d4afc62e2544ad013949954819fe3675af5dcf
cococolor <819109255@qq.com>
2022-11-24 11:53:46 +0800

    launch立即调用

--- src/index.js
+++ src/index.js
@@ -126,7 +126,17 @@ function initWechatGameAppProxy() {
       if (!turbo._meta.life_state.app_launched) {
         if (turbo?._para?.autoTrack?.appLaunch) {
           const option = wx.getLaunchOptionsSync() || {};
-          turbo._autoTrackCustom.appLaunch(option);
+          // turbo._autoTrackCustom.appLaunch(option);
+          sendOnce({
+            type: "track",
+            event: "$MPLaunch",
+            properties: {
+              $is_first_time: turbo._is_first_launch,
+              $url_query: setQuery(option.query),
+              $scene: String(option.scene),
+            },
+            time: Date.now(),
+          });
         }
       }
       turbo._autoTrackCustom.appShow(para);
@@ -141,12 +151,25 @@ function initWechatGameAppProxy() {
 
 function checkAppLaunch() {
   if (!turbo._meta.life_state.app_launched) {
-    const option = {};
+    let option = {};
+    if (Laya.Browser.onMiniGame) {
+      option = wx.getLaunchOptionsSync() || {};
+    }
     turbo._meta.life_state.app_launched = true;
-    turbo._current_scene = option.scene;
-    turbo._autoTrackCustom.appLaunch({
-      query: option?.query || {},
-      scene: option.scene || "",
+    turbo._current_scene = option?.scene;
+    // turbo._autoTrackCustom.appLaunch({
+    //   query: option?.query || {},
+    //   scene: option?.scene || "",
+    // });
+    sendOnce({
+      type: "track",
+      event: "$MPLaunch",
+      properties: {
+        $is_first_time: turbo._is_first_launch,
+        $url_query: setQuery(option?.query || {}),
+        $scene: String(option.scene || ""),
+      },
+      time: Date.now(),
     });
   }
 }
@@ -189,6 +212,26 @@ turbo.init = function (access_token = "", client_id = "") {
     initWechatGameAppProxy();
   }
 };
+
+function sendOnce(data) {
+  if (!turbo._globalData.client_id) {
+    return;
+  }
+  turbo._miniGamePlatform = getMiniGamePlatform();
+  const datas = turbo._store.mem.getMultList([data]) || [];
+  const url =
+    turbo._para.server_url + `?access_token=${turbo._globalData.access_token}`;
+  if (!Laya.Browser.onMiniGame) {
+    xhrPromise(url, datas[0], "POST");
+    return;
+  }
+  turbo.platform_obj.request({
+    url,
+    method: "POST",
+    header,
+    data: datas[0],
+  });
+}
 
 const sendStrategy = {
   dataHasSend: true,
