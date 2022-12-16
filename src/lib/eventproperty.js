049e1b1f32ed8dc1a98cadcca9a9f841aa644f9e
pony <luckyliangfei@gmail.com>
2022-12-16 12:14:14 +0800

    3.3.1 publish

--- src/lib/eventproperty.js
+++ src/lib/eventproperty.js
@@ -53,7 +53,7 @@ export const eventProperty = {
           $model: info.model,
           $brand: String(info.brand).toLocaleUpperCase(),
           $manufacturer: info.brand,
-          $lib_version: "3.2.8",
+          $lib_version: "3.3.1",
           $lib: "Laya",
         };
         for (let item in temp_systeminfo) {
